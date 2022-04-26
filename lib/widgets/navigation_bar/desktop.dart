33bf6bc3f4e530875e42af29d3f4c43e3e2114d3
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-04-26 22:36:20 +0300

    Widgets

---
+++ lib/widgets/navigation_bar/desktop.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+import 'package:radio_ben_zvi_website/pages/stateless_page.dart' as RBZPages;
+import 'package:radio_ben_zvi_website/widgets/navigation_bar/items/logo.dart';
+
+class NavigationBarDesktop extends RBZPages.Page 
+{
+    const NavigationBarDesktop({Key? key, required ToPage}) : super(key: key, ToPage: ToPage);
+
+    @override
+    Widget build(BuildContext context) 
+    {
+        return SizedBox(
+            height: 100,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    NavBarLogo(ToPage: ToPage)
+                ]
+            )
+        );
+    }
+}
---
+++ lib/widgets/navigation_bar/items/logo.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: deprecated_member_use, non_constant_identifier_names, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'package:radio_ben_zvi_website/globals.dart' as g;
+import 'package:radio_ben_zvi_website/pages/state_page.dart' as RBZPages;
+import 'package:radio_ben_zvi_website/pages/home/page.dart';
+
+class NavBarLogo extends RBZPages.Page
+{
+    const NavBarLogo({Key? key, required ToPage}) : super(key: key, ToPage: ToPage);
+
+    @override
+    State<NavBarLogo> createState() => _NavBarLogoState();
+}
+
+// _NavBarLogoState is a state class for the NavBarLogo widget.
+// extend the State class to create a state class for a stateful widget.
+// with the extension HoverExtensions
+class _NavBarLogoState extends State<NavBarLogo>
+{
+    bool isHover = false;
+
+    @override
+    Widget build(BuildContext context)
+    {
+        final HoveredTransform = Matrix4.identity()
+            ..scale(1.2)
+            ..translate(-5, -3, 0);
+        final ButtonTransform = isHover
+            ? HoveredTransform
+            : Matrix4.identity();
+
+        return SizedBox(
+            height: 100,
+            width: 100,
+            child: MouseRegion(
+                onHover: (event) => setState(() => isHover = true),
+                onExit: (event) => setState(() => isHover = false),
+                child: AnimatedContainer(
+                    duration: Duration(milliseconds: 200),
+                    transform: ButtonTransform,
+                    child: FlatButton(
+                        color: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        shape: StadiumBorder(),
+                        child: Image.asset('assets/images/logo/logo.png'),
+                        onPressed: () => widget.ToPage(Home(ToPage: widget.ToPage))
+                    )
+                )
+            )
+        );
+    }
+}
