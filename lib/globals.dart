3dd99beac220c540bb1ddfdaf227b98985e85c72
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-04-26 22:35:20 +0300

    main

---
+++ lib/globals.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+Map podcasts_names = {
+    "11": "podcastWithoutaName",
+    "12": "playlistWithoutaName",
+};
+int chosen_podcast = 11;
+
+// hero anim tags:
+const Map hero_anim_tags = {};
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,34 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_init_to_null, unnecessary_null_comparison, prefer_const_declarations, use_key_in_widget_constructors, library_prefixes
+
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+import 'package:radio_ben_zvi_website/globals.dart' as g;
+import 'package:radio_ben_zvi_website/OORT/app_data.dart' as data;
+import 'package:radio_ben_zvi_website/OORT/navigator/navigator.dart' as Navigator;
+
+void main() => runApp(RadioBenZviWebsite());
+
+class RadioBenZviWebsite extends StatefulWidget 
+{
+    const RadioBenZviWebsite({ Key? key }) : super(key: key);
+
+    @override
+    State<RadioBenZviWebsite> createState() => _RadioBenZviWebsiteState();
+}
+
+class _RadioBenZviWebsiteState extends State<RadioBenZviWebsite> 
+{
+    @override
+    Widget build(BuildContext context) 
+    {
+        return MaterialApp.router(
+            title: 'Radio Ben Zvi Website',
+            routerDelegate: Navigator.Delegate,
+            routeInformationParser: Navigator.InformationParser,
+            themeMode: ThemeMode.dark,
+            theme: ThemeData(
+                fontFamily: data.MainFont
+            ),
+        );
+    }
+}
