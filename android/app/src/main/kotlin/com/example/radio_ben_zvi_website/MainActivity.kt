ebe441dca2016c304d52537211b639be9aee3e91
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-05-18 22:37:45 +0300

    a

--- android/app/src/main/kotlin/com/example/radio_ben_zvi_website/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.radio_ben_zvi_website
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- build.py
+++
@@ -1,4 +0,0 @@
-from os import system
-
-system("flutter build web --web-renderer=html")
-print("built the Radio Ben Zvi website")
