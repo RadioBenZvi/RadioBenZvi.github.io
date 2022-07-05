d5052bb1bce0e685c3d1aff95c6b8dca35eba810
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-07-05 12:30:32 +0300

    Publishing the Radio BenZvi website

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "404.html": "f5957a46e21610d3b8fc7e5ebae56514",
+  "2.html": "f5957a46e21610d3b8fc7e5ebae56514",
 "404.jpg": "0d93dd411e3bc82dbae4ffc419a317cb",
 "assets/AssetManifest.json": "e3fd2eb61c6baad82b5e250031971e0a",
 "assets/assets/fonts/DorianCLM-Book.ttf": "bb7dfe9bdb231df99b884d9ef5f87b87",
@@ -67,9 +67,9 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "ff9f7efb4e036eece58263da8cb4b398",
 "icons/ms-icon-310x310.png": "4011ddc7a46d3b9132bf34bcbe943bc9",
 "icons/ms-icon-70x70.png": "85fb0cf4469bfcccc5e1851aafa2b44d",
-"index.html": "3b5d15ad6544c9231731cf0de466e4e5",
-"/": "3b5d15ad6544c9231731cf0de466e4e5",
-"main.dart.js": "fb737966e89474a6a3aa61db32e0a0a3",
+"index.html": "fe797a28af743a8cdaa1e142d15b0a3b",
+"/": "fe797a28af743a8cdaa1e142d15b0a3b",
+"main.dart.js": "3744dc964d7cf8f05cda62421987d422",
 "manifest.json": "9d63757af368f9cf8511459e042310e5",
 "move.py": "abdd39a067cf4ddcde4f305610f328b8",
 "PAWS1.html": "d2c8c3916d59f25ed9f93efa107bdb81",
