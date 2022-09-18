a73422a36ff27d8084ec31648878dd7ebaeeb9a3
ShakedKod <82947988+Shakedkod@users.noreply.github.com>
2022-09-18 22:16:00 +0300

    TheUpdate

---
+++ upload.py
@@ -0,0 +1,32 @@
+from os import system
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import date, datetime
+from time import sleep
+
+global theJobDone
+theJobDone = False
+sched = BackgroundScheduler()
+
+def theJob():
+    print("Executing Task...")
+
+    system("git add .")
+    system("git commit -m \"TheUpdate\"")
+    system("git push")
+
+    print("Task completed successfully")
+    global theJobDone
+    theJobDone = True
+
+sched.add_job(
+    theJob,
+    'date',
+    run_date = datetime(2022, 9, 18, 22, 16, 0) #datetime(2022, 9, 20, 13, 0, 0)
+)
+
+# Starts the Scheduled jobs
+sched.start()
+
+# Runs an infinite loop
+while not theJobDone:
+    sleep(1)
